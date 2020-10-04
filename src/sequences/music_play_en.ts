ca2eba9af28f2aaf204460eab696dbc5a8be47f1
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-10-04 00:53:35 -0400

    Add documentation and comments

--- src/sequences/music_play_en.ts
+++ src/sequences/music_play_en.ts
@@ -3,17 +3,17 @@ import { _artist_, _song_, _album_, _genre_ } from "../rulenames/music";
 /* Util */
 import array from "../util/arrayUtils";
 
-// I want to, I would like to ~
+// BEGIN: Generate "I want to, I would like to ~"
 const desire_verbs: string[] = ["want", "would like"];
 const play_verbs_infinitive: string[] = ["listen to", "hear"];
-
 const play_request_infinitive = (): string => {
   const _desire_verb: string = array.rndElem(desire_verbs);
   const _play_verb_infinitive: string = array.rndElem(play_verbs_infinitive);
   return `I ${_desire_verb} to ${_play_verb_infinitive}`;
 };
+// END: Generate "I want to, I would like to ~"
 
-// Play me, put on for me ~
+// BEGIN: Generate "Play me, put on for me ~"
 const polite_request_verbs: string[] = ["could", "would", "can"];
 const play_verbs_imperative: string[] = ["play", "put on"];
 const play_request_imperative = (): string => {
@@ -21,12 +21,16 @@ const play_request_imperative = (): string => {
   const _play_verb_imperative = array.rndElem(play_verbs_imperative);
   return `${_polite_request_verb} you ${_play_verb_imperative}`;
 };
+// END: Generate "Play me, put on for me ~"
 
 export default (): string => {
+  // This is used when generating a random request in infinitive or imperative form
   const rnd_request: string[] = [
     play_request_infinitive(),
     play_request_imperative(),
   ];
+
+  // This is used when generating a random subject from a compiled list of artists, songs, etc
   const rnd_subject: string[] = [
     ..._artist_,
     ..._song_,
@@ -34,6 +38,7 @@ export default (): string => {
     ..._genre_.map((genre) => `${genre} music`),
   ];
 
+  // Return a randomly generated sentence based on the arrays above
   const sentence: string = `${array.rndElem(rnd_request)} ${array.rndElem(
     rnd_subject
   )}`;
--- src/sequences/music_play_jp.ts
+++ src/sequences/music_play_jp.ts
@@ -1,1 +1,14 @@
+/* Constants */
+import { _artist_, _song_, _album_, _genre_ } from "../rulenames/music";
+/* Util */
+import array from "../util/arrayUtils";
+
+// conjugations and endings
+const want_verb_ending = ["tai"];
+const command_ending = ["te", "te kudasai", "ro"];
+const request_ending = ["te hoshii", "te kureru", "te kurenai"];
+const base_conjugation = "shi";
+
+// I want to, I would like to ~
+
 export default () => {};
