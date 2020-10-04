4b4e3c5cf1316ccffb1510564acb1396061ebf15
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-10-03 20:30:37 -0400

    Add math util functions and btn to display generated sentence

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+/* Util */
+import math from "./util/mathUtils";
 
 function App() {
+  /* State */
+  const [sentence, setSentence] = useState<string>("test");
+
+  const handleGenerateSentence = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setSentence(math.rndInt(1, 2).toString());
+  };
+
   return (
     <div className="App">
       <h3>setup</h3>
+
+      <p>{sentence}</p>
+
+      <button onClick={handleGenerateSentence}>Generate sentence</button>
     </div>
   );
 }
---
+++ src/util/mathUtils.ts
@@ -0,0 +1,10 @@
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export default {
+  rndInt: getRandomIntInclusive,
+};
